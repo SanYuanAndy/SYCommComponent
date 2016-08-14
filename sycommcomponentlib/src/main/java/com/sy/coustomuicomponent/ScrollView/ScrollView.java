3e73103dcc7a5f1af41f07adc3a59486c37813e8
sanyuanzhao <765828478@qq.com>
2016-08-14 23:42:53 +0800

    change scrollview

--- sycommcomponentlib/src/main/java/com/sy/coustomuicomponent/ScrollView/ScrollView.java
+++ sycommcomponentlib/src/main/java/com/sy/coustomuicomponent/ScrollView/ScrollView.java
@@ -10,10 +10,23 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.util.EventListenerProxy;
 
 /**
  * Created by ASUS User on 2016/8/11.
+ * Note：
+ * 两个概念：
+ * 1、可视视图：真实呈现在用户眼前的可视区域。大小、位置固定。
+ * 2、内存视图：真实视图的数据表现形式。大小、位置可以变化。
+     **************
+   --*------------*---
+   - *            *  -
+   - *            *  -
+   - **************  -
+   -------------------
+
+    *号所围成的区域是可视视图。-号所围成的区域是内存视图。两者重叠的部分是，
+ *  用户能够真正能够看到的内存视图的一部分。移动或者改变内存视图，用户就可以
+ *  看到内存视图的其他部分或者全部内容。
  */
 public class ScrollView extends View implements View.OnTouchListener{
     public ScrollView(Context context){
@@ -27,10 +40,12 @@ public class ScrollView extends View implements View.OnTouchListener{
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d("", "widthMeasureSpec " + widthMeasureSpec);
+        Log.d("", "heightMeasureSpec " + heightMeasureSpec);
         Log.d("", "width " + getWidth());
         Log.d("", "height " + getHeight());
         //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(1080 + 360, 360);
+        setMeasuredDimension(1080 + 360*2, 360);
     }
 
     @Override
@@ -38,11 +53,10 @@ public class ScrollView extends View implements View.OnTouchListener{
         super.onDraw(canvas);
         Paint paint = new Paint();
         Rect rect = new Rect();
+        int perWitdh = getWidth()/mChunkCount;
         int top = getTop();
         int bottom = getBottom();
-        int right = getRight();
-        int left = getLeft();
-        int perWitdh = getWidth()/4;
+        int left = getLeft() - perWitdh;
 
         paint.setColor(Color.RED);
         rect.set(left, top, left + perWitdh, bottom);
@@ -59,29 +73,35 @@ public class ScrollView extends View implements View.OnTouchListener{
         canvas.drawRect(rect, paint);
 
         left += perWitdh;
+        paint.setColor(Color.GRAY);
+        rect.set(left, top, left + perWitdh, bottom);
+        canvas.drawRect(rect, paint);
+
+        left += perWitdh;
         paint.setColor(Color.YELLOW);
         rect.set(left, top, left + perWitdh, bottom);
         canvas.drawRect(rect, paint);
     }
 
-    private float oldPotion_x = 0;
-
-
+    private int mChunkCount = 5;
+    private float oldPotionX = 0;
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         switch (motionEvent.getAction()){
             case MotionEvent.ACTION_UP: {
-                int moveStep = (int) (oldPotion_x - motionEvent.getX());
+                int moveStep = (int) (motionEvent.getX() - oldPotionX);
                 scrollBySafety(moveStep, 0, true);
             }
                 break;
             case MotionEvent.ACTION_DOWN: {
-                oldPotion_x = motionEvent.getX();
+                oldPotionX = motionEvent.getX();
             }
                 break;
             case MotionEvent.ACTION_MOVE: {
-                int moveStep = (int) (motionEvent.getX() - oldPotion_x);
+                float currPotionX = motionEvent.getX();
+                int moveStep = (int) (currPotionX - oldPotionX);
                 scrollBySafety(moveStep, 0, false);
+                oldPotionX = currPotionX;
             }
                 break;
             default:
@@ -89,24 +109,23 @@ public class ScrollView extends View implements View.OnTouchListener{
         return true;
     }
 
-    private int currX = getLeft();
-    private int currY = getTop();
+    private int currX = 0;
+    private int currY = 0;
     private void scrollBySafety(int x, int y, boolean bRestore){
-        int newX = currX + x;
-        Log.d("", "currX : " + currX + ",newX : " + newX);
-        if (newX >= -1 * getWidth()/4 && newX <= getWidth()/4) {
-            scrollBy(x, y);
+        int newX = currX - x;
+        if (newX >= -1 * getWidth()/mChunkCount * 2 && newX <= getWidth()/mChunkCount * 2) {
+            scrollBy(-x, y);
             currX = newX;
         }
         if (bRestore) {
-            if (newX >= getWidth() / 4) {
-                newX = getWidth() / 4;
+            if (newX >= getWidth() / mChunkCount) {
+                newX = getWidth() / mChunkCount;
                 scrollTo(newX, currY);
                 currX = newX;
             }
 
-            if (newX <= -getWidth() / 4) {
-                newX = -getWidth() / 4;
+            if (newX <= -getWidth() / mChunkCount) {
+                newX = -getWidth() / mChunkCount;
                 scrollTo(newX, currY);
                 currX = newX;
             }
